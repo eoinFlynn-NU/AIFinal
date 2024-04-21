33e6ad819c67cca3e9d4aed7bbe5eb16cc6e3543
adam-bello <72825559+adam-bello@users.noreply.github.com>
2024-04-21 16:45:40 -0400

    cleanup + weights folder

--- adam.py
+++ adam.py
@@ -4,6 +4,7 @@ import numpy as np
 import gymnasium as gym
 from gymnasium.wrappers import FrameStack
 from gymnasium.wrappers import GrayScaleObservation
+from gymnasium.wrappers import AtariPreprocessing
 
 from matplotlib import pyplot as plt
 from collections import deque, namedtuple
@@ -22,22 +23,17 @@ gamma = 0.99
 epsilon_i = 1.0
 epsilon_f = 0.01
 epsilon_d = 0.995
-replay_buffer_size = 10000
+replay_buffer_size = 1000
 batch_size = 32
 learning_rate = 0.01
-# frame_stack = 4
-# Use num_lives to reset after each life (maybe)
-num_lives = 4
 
 
-env = gym.make("ALE/Assault-v5", render_mode='rgb_array')
-env = GrayScaleObservation(env, keep_dim=True)
+env = gym.make("ALE/SpaceInvaders-v5", render_mode='human', frameskip=1)
+env = AtariPreprocessing(env=env, noop_max=30, grayscale_newaxis=True, grayscale_obs=True, screen_size=84)
 states = env.observation_space.shape
 actions = env.action_space.n
-# observation_space = Box(0, 255, (4, 210, 160), uint8)
 
 class AssaultNet(nn.Module):
-
   def __init__(self, num_frames, num_actions):
     super(AssaultNet, self).__init__()
     self.num_frames = num_frames
@@ -77,18 +73,13 @@ class AssaultNet(nn.Module):
       return fake_output.view(-1).size(0)
   
   def forward(self, x):
-
     x = self.relu(self.conv1(x))
-    # print(np.array(x).shape)
     x = self.relu(self.conv2(x))
-    # print(np.array(x).shape)
     x = x.view(-1, self.flatten_size)
-    # print(np.array(x).shape)
     x = self.relu(self.fc1(x))
     x = self.relu(self.fc2(x))
     x = self.fc3(x)
     return x
-
 class Agent:
   def __init__(self, model, target_update, learning_rate, gamma, epsilon_i, epsilon_f, epsilon_d, batch_size):
     self.model = model
@@ -112,8 +103,6 @@ class Agent:
     
   @staticmethod
   def _process_obs(obs):
-    obs = cv2.resize(obs, (84, 84), interpolation=cv2.INTER_AREA)
-    obs = obs[:, :, np.newaxis]
     obs = obs.transpose((2, 0, 1))
     return obs
 
@@ -126,17 +115,19 @@ class Agent:
         return q_values.argmax().item()
 
   def train(self, num_episodes):
+    rewards = []
+    epsilons = []
     for episode in range(num_episodes):
       state, info = env.reset()
+      self.lives = info['lives']
       state = self._process_obs(np.array(state))
       episode_reward = 0
       terminated = False
+      steps = 0
       while not terminated:
         action = self.select_action(state)
         next_state, reward, terminated, truncated, info = env.step(action)
         next_state = self._process_obs(np.array(next_state))
-        # print(episode_reward)
-        # print(info['lives'])
         episode_reward += reward
         self.memory.append((np.array(state), action, reward, np.array(next_state), terminated))
         state = next_state
@@ -146,15 +137,23 @@ class Agent:
         
         if len(self.memory) > 1000:
           self.memory = []
+        
+        if steps == self.target_update:
+          self.target_model.load_state_dict(self.model.state_dict())
+          steps = 0
       
       self.epsilon_i = max(self.epsilon_f, self.epsilon_i * self.epsilon_d)
+      
+      epsilons.append(self.epsilon_i)
+      rewards.append(episode_reward)
 
-      if episode % self.target_update == 0:
-        self.target_model.load_state_dict(self.model.state_dict())
+      
 
       print(f"Episode: {episode}, Reward: {episode_reward}, Epsilon: {self.epsilon_i}")
     
-    torch.save(self.model.state_dict(), "assault_DQN_CNN_1T.pt")
+    # Uncomment to save reward + epsilon values from training to csv files
+    # np.savetxt("si_science_rewards.csv", np.asarray(rewards), delimiter=",")
+    # np.savetxt("si_science_epsilons.csv", np.asarray(epsilons), delimiter=",")
 
   def update_model(self):
     batch = np.random.choice(len(self.memory), self.batch_size, replace=False)
@@ -175,10 +174,6 @@ class Agent:
     target_q_values = rewards + (1 - dones) * self.gamma * next_q_values
     selected_q_values = q_values.gather(1, actions.unsqueeze(1))
 
-    # print("q_values.requires_grad:", q_values.requires_grad)
-    # print("actions.requires_grad:", actions.requires_grad)
-    # print("selected_q_values.requires_grad:", selected_q_values.requires_grad)
-
     loss = F.smooth_l1_loss(selected_q_values, target_q_values.unsqueeze(1))
 
     self.optimizer.zero_grad()
@@ -186,33 +181,56 @@ class Agent:
     self.optimizer.step()
   
   def save_model(self):
-    torch.save(self.target_model.state_dict(), 'target_model.pth')
+    torch.save(self.target_model.state_dict(), 'fw_5H_science.pth')
   
   def load_model(self):
-    self.model.load_state_dict(torch.load('target_model.pth'))
+    self.model.load_state_dict(torch.load('si_DQN_CNN_2T_science.pt'))
     model.eval()
 
 model = AssaultNet(1, actions)
 agent = Agent(model, target_update, learning_rate, gamma, epsilon_i, epsilon_f, epsilon_d, batch_size)
-agent.train(1000)
-agent.save_model()
-
+# agent.train(2000)
+# agent.save_model()
 
 # After training, you can evaluate the performance of the trained agent
-def evaluate_agent(agent, num_episodes=3):
-    total_rewards = []
-    for episode in range(num_episodes):
-        state = env.reset()
-        episode_reward = 0
-        done = False
-        while not done:
-            action = agent.select_action(state)
-            next_state, reward, done, truncated, _ = env.step(action)
-            episode_reward += reward
-            state = next_state
-        total_rewards.append(episode_reward)
-    average_reward = sum(total_rewards) / num_episodes
-    print(f"Average reward over {num_episodes} episodes: {average_reward}")
+def evaluate_agent(num_episodes, weight_file):
+    model = AssaultNet(1, actions)
+    model.load_state_dict(torch.load(weight_file))
+    model.eval()
+    rewards = []
 
-# Evaluate the trained agent
-# evaluate_agent(agent)
+    for episode in range(num_episodes):
+        state, _ = env.reset()
+        episode_reward = 0
+        terminated = False
+        truncated = False
+
+        while(not terminated and not truncated):  
+          # Ensure state is a valid image
+          if state is None:
+              print("Invalid state received. Skipping episode.")
+              break
+
+          # Resize and normalize the state
+          state = cv2.resize(state, (84, 84))
+          state = np.array(state, dtype=np.float32) / 255.0
+
+          # Convert state to tensor and add batch dimension
+          state_tensor = torch.tensor(state, dtype=torch.float32).unsqueeze(0).unsqueeze(0)
+
+          # Select best action   
+          with torch.no_grad():
+            action = model(state_tensor).argmax().item()
+
+          # Execute action
+          state,reward,terminated,truncated,_ = env.step(action)
+          episode_reward += reward
+
+        
+        rewards.append(episode_reward)
+        print(f"Episode: {episode}, Reward: {episode_reward}")
+    np.savetxt("trained_rewards.csv", np.asarray(rewards), delimiter=",")
+    env.close()
+
+weight_file = '' # path to weight file for evaluation 
+evaluate_agent(50, weight_file)
