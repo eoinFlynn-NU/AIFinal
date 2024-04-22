bd836c782eeccdd3aee71d6e0d3da5ebb4de9460
Eoin Flynn <flynn.j@northeastern.edu>
2024-04-21 20:53:29 -0400

    main and assault updated

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from assault_overheat_update import train_assault_agent, evaluate_assault_agent
+from assault_model import train_assault_agent, evaluate_assault_agent
 import argparse
 
   
@@ -8,17 +8,26 @@ def main():
   parser = argparse.ArgumentParser(description='Train or evaluate agents in different atari environments')
   parser.add_argument('env', type=str, help='Name of the atari environment(assault, freeway, or space_invaders)')
   parser.add_argument('mode', type=str, help='train or evaluate')
-  parser.add_argument('--train_episodes', type=int, default=1000, help='Number of episodes to train the agent')
-  parser.add_argument('--save_file_name', type=str, default='assault_model.pth', help='File name to save the trained model')
-  parser.add_argument('--eval_episodes', type=int, default=50, help='Number of episodes to evaluate the agent')
-  parser.add_argument('--load_file_name', type=str, default='assault_model.pth', help='File name to load the trained model')
+  parser.add_argument('--episodes', type=int, default=1000, help='Number of episodes to train or evaluate the agent')
+  parser.add_argument('--save_file_name', type=str, help='File name to save the trained model')
+  parser.add_argument('--load_file_name', type=str, help='File name to load the trained model')
   args = parser.parse_args()
 
   if args.env == 'assault':
     if args.mode == 'train':
-      train_assault_agent(args.num_episodes, args.save_file_name)
+      train_assault_agent(args.episodes, args.save_file_name)
     elif args.mode == 'evaluate':
-      evaluate_assault_agent(args.load_file_name, args.num_eval_episodes)
+      evaluate_assault_agent(args.load_file_name, args.episodes)
+  if args.env == 'freeway':
+    if args.mode == 'train':
+      train_freeway_agent(args.episodes, args.save_file_name)
+    elif args.mode == 'evaluate':
+      evaluate_freeway_agent(args.load_file_name, args.episodes)
+  if args.env == 'space_invaders':
+    if args.mode == 'train':
+      train_space_invaders_agent(args.episodes, args.save_file_name)
+    elif args.mode == 'evaluate':
+      evaluate_space_invaders_agent(args.load_file_name, args.episodes)
 
 
 if __name__ == "__main__":
